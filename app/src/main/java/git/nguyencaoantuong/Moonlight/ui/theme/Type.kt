ae812fb1a7d8aee579ed6c482484f9791c7db52c
ncatt <anntuongg@gmail.com>
2026-01-17 13:40:48 +0700

    Update Type.kt

--- app/src/main/java/git/nguyencaoantuong/Moonlight/ui/theme/Type.kt
+++ app/src/main/java/git/nguyencaoantuong/Moonlight/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package git.nguyencaoantuong.Moonlight.ui.theme
+package org.universalUI.universalX.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
